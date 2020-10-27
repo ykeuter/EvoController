9ca197b99cca31ace90a6e5b6a0f3e73147a1f33
Yvo Keuter <ypmkeuter@gmail.com>
2020-10-27 20:01:35 +0100

    demo finetuning

--- scripts/hello_drone.py
+++ scripts/hello_drone.py
@@ -1,6 +1,17 @@
 import airsim
+import math
+import time
 
 client = airsim.MultirotorClient()
+
+
+def waitAndGetHealth(seconds):
+    print("Health: {}".format(client.simGetHealth()), end="\r")
+    for _ in range(math.ceil(seconds)):
+        time.sleep(1)
+        print("Health: {}".format(client.simGetHealth()), end="\r")
+
+
 client.confirmConnection()
 client.reset()
 
@@ -8,9 +19,12 @@ client.enableApiControl(True)
 client.armDisarm(True)
 client.takeoffAsync().join()
 
-client.moveByVelocityAsync(0, 1, 0, 13).join()
-client.moveByVelocityAsync(0, -1, 0, 32).join()
-client.moveByVelocityAsync(0, 1, 0, 19).join()
+client.moveByVelocityAsync(0, 1, 0, 13)
+waitAndGetHealth(1.5 * 13 / 3)
+client.moveByVelocityAsync(0, -1, 0, 30)
+waitAndGetHealth(1.5 * 30 / 3)
+client.moveByVelocityAsync(0, 1, 0, 17)
+waitAndGetHealth(1.5 * 17 / 3)
 
 client.landAsync().join()
 client.armDisarm(False)
