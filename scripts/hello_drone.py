bb29e9e75002fade587a8f7fbae56700b6e504ee
Yvo Keuter <ypmkeuter@gmail.com>
2020-10-27 10:45:54 +0100

    prepared demo script

--- scripts/hello_drone.py
+++ scripts/hello_drone.py
@@ -1,25 +1,17 @@
-# import setup_path 
 import airsim
 
-import numpy as np
-
-# connect to the AirSim simulator
 client = airsim.MultirotorClient()
 client.confirmConnection()
 client.reset()
+
 client.enableApiControl(True)
 client.armDisarm(True)
-# 
 client.takeoffAsync().join()
-client.moveByVelocityAsync(1, 0, 0, 50).join()
-# client.moveByVelocityAsync(0, 5, 0, 5).join()
-# client.moveByVelocityAsync(0, 0, 5, 5).join()
 
-responses = client.simGetImages([
-    airsim.ImageRequest("front", airsim.ImageType.Scene, True)
-])  #scene vision image in uncompressed RGBA array
-response = responses[0]
-print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
-airsim.write_pfm('test.pfm', airsim.get_pfm_array(response))
+client.moveByVelocityAsync(0, 1, 0, 13).join()
+client.moveByVelocityAsync(0, -1, 0, 32).join()
+client.moveByVelocityAsync(0, 1, 0, 19).join()
 
+client.landAsync().join()
+client.armDisarm(False)
 client.enableApiControl(False)
