3c991e823d20376ad28638150b79731fc0c3d603
Yvo Keuter <ypmkeuter@gmail.com>
2021-06-07 03:37:05 +0200

    genotype

--- evo_controller/population/alpha.py
+++ evo_controller/population/alpha.py
@@ -1,6 +1,7 @@
 from neat import DefaultGenome
 from configparser import ConfigParser
 from .base_population import BasePopulation
+from .agent import Agent
 
 
 class Alpha(BasePopulation):
@@ -23,3 +24,15 @@ class Alpha(BasePopulation):
         print("terminate")
         for id in terminal_steps.agent_id:
             del self.agents[id]
+
+    def add_agent(self, id, parent1_id, parent2_id):
+        # parent2 is not supported
+        genome = self.genome_type(id)
+        if parent1_id == 0:
+            genome.configure_new(self.genome_config)
+        else:
+            parent = self.agents[parent1_id].genotype
+            genome.configure_crossover(parent, parent, self.genome_config)
+            genome.mutate(self.genome_config)
+        # phenotype
+        self.agents[id] = Agent(genome, phenotype)
