a706e2afff1f6101eca39f582d4614051a151370
Yvo Keuter <ypmkeuter@gmail.com>
2021-07-06 19:25:06 +0200

    adding analyzer

---
+++ evo_controller/analyzers/log_analyzer.py
@@ -0,0 +1,57 @@
+import csv
+from collections import Counter
+from operator import index
+import pandas as pd
+import numpy as np
+
+
+class LogAnalyzer:
+    def read(self, fn):
+        with open(fn) as f:
+            reader = csv.reader(f, delimiter="|")
+            nbirth = 0
+            ndeath = 0
+            counter = Counter()
+            df = pd.DataFrame(columns=["age", "births", "deaths"])
+            for row in reader:
+                if row[2] == "age":
+                    d = {
+                        "age": int(float(row[3])),
+                        "births": nbirth,
+                        "deaths": ndeath
+                    }
+                    df = df.append(d, ignore_index=True)
+                    nbirth = 0
+                    ndeath = 0
+                elif row[2] == "birth":
+                    nbirth += 1
+                    counter[int(row[4])] += 1
+                elif row[2] == "death":
+                    ndeath += 1
+                else:
+                    raise ValueError
+
+        ma = max(counter.keys())
+        df2 = pd.DataFrame(
+            columns=["id", "min", "q1", "med", "q3", "max", "avg"])
+        for i in range(1000, ma, 1000):
+            a = [counter[j] for j in range(i - 1000, i)]
+            q = np.quantile(a, [0, .25, .5, .75, 1])
+            d = {
+                "id": i,
+                "min": q[0],
+                "q1": q[1],
+                "med": q[2],
+                "q3": q[3],
+                "max": q[4],
+                "avg": np.mean(a)
+            }
+            df2.append(d, ignore_index=True)
+
+        self.population_stats = df
+        self.offspring_stats = df2
+
+
+def write(self, population_fn, offspring_fn):
+    self.population_stats.to_csv(population_fn, index=False)
+    self.offspring_stats.to_csv(offspring_fn, index=False)
--- sandbox.py
+++ sandbox.py
@@ -1,27 +1,5 @@
-import logging
-import logging.config
-import json
-from neat import DefaultGenome
-from configparser import ConfigParser
-from evo_controller.codecs.default_genome_encoder import DefaultGenomeEncoder
+from evo_controller.analyzers.log_analyzer import LogAnalyzer
 
-
-with open("log_config.json") as fp:
-    logging.config.dictConfig(json.load(fp))
-
-logging.getLogger("evo_controller").info(123)
-
-parameters = ConfigParser()
-with open("config/alpha.cfg") as f:
-    parameters.read_file(f)
-genome_dict = dict(parameters.items(DefaultGenome.__name__))
-genome_config = DefaultGenome.parse_config(genome_dict)
-
-genome = DefaultGenome(777)
-genome.fitness = -1
-genome.configure_new(genome_config)
-
-print(genome.__dict__)
-logging.getLogger("evo_controller").info(
-    "{}".format(DefaultGenomeEncoder(separators=(",", ":")).encode(genome))
-)
+a = LogAnalyzer()
+a.read("evo.log")
+a.write("population.csv", "offspring.csv")
