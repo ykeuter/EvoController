ba070e3b8bafdc563e1b6c31160f037e65525c9b
Yvo Keuter <ypmkeuter@gmail.com>
2021-07-14 18:07:49 +0200

    fix decoder

--- evo_controller/codecs/default_genome_decoder.py
+++ evo_controller/codecs/default_genome_decoder.py
@@ -3,30 +3,21 @@ from neat.genes import DefaultConnectionGene, DefaultNodeGene
 
 
 class DefaultGenomeDecoder():
-    def __init__(self, config):
-        self.config = config
-
-
-def as_default_genome(self, dct):
-    g = neat.DefaultGenome(dct["key"])
-    g.fitness = dct["fitness"]
-    g.nodes = [
-        self.create_node(d) for d in dct["nodes"]
-    ]
-    g.connections = [
-        self.create_connection(d) for d in dct["connections"]
-    ]
-
-
-def create_node(self, dct):
-    n = DefaultNodeGene(dct["key"])
-    for a in n._gene_attributes:
-        setattr(n, a.name, dct[a.name])
-    return n
-
-
-def create_connection(self, dct):
-    c = DefaultConnectionGene(dct["key"])
-    for a in c._gene_attributes:
-        setattr(c, a.name, dct[a.name])
-    return c
+    def as_default_genome(self, dct):
+        if "activation" in dct:
+            n = DefaultNodeGene(dct["key"])
+            for a in n._gene_attributes:
+                setattr(n, a.name, dct[a.name])
+            return n
+        if "from" in dct:
+            c = DefaultConnectionGene((dct["from"], dct["to"]))
+            for a in c._gene_attributes:
+                setattr(c, a.name, dct[a.name])
+            return c
+        if "fitness" in dct:
+            g = neat.DefaultGenome(dct["key"])
+            g.fitness = dct["fitness"]
+            g.nodes = {n.key: n for n in dct["nodes"]}
+            g.connections = {c.key: c for c in dct["connections"]}
+            return g
+        return dct
--- sandbox.py
+++ sandbox.py
@@ -1,17 +1,10 @@
-from evo_controller.analyzers.log_analyzer import LogAnalyzer
-import csv
+from evo_controller.codecs.default_genome_decoder import DefaultGenomeDecoder
+import json
 
-a = LogAnalyzer()
-a.read("evo.log")
-a.write("population.csv", "offspring.csv")
 
-# with open("evo.log") as f:
-#     reader = csv.reader(f, delimiter="|")
-#     for row in reader:
-#         # print(row)
-#         if row[2] == "age":
-#             print("age {}".format(row[3]))
-#         elif row[2] == "birth":
-#             id = int(row[3])
-#             if id > 821700 and id < 821750:
-#                 print("birth {} {}".format(row[3], row[4]))
+decoder = DefaultGenomeDecoder()
+with open("genomes.txt") as f:
+    for line in f:
+        print("line:")
+        d = json.loads(line)
+        g = json.loads(line, object_hook=decoder.as_default_genome)
